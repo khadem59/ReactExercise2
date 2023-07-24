8cfd4a227f8399b173ca06bc49f3e2bea902bcf1
khadem59 <fkhademi187@gmail.com>
2023-07-24 13:25:32 -0700

    add all files

---
+++ my-site/src/components/AboutUs.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import style from "../styles/style.module.css";
+
+
+class AboutUs extends Component {
+  render() {
+    return (
+  <>
+   <h1>درسمن تیمی تشکیل شده از مدرسان متخصص در زمینه‌ی 
+    برنامه نویسی  می‌باشد</h1>
+    <h4>اینجانب فاطمه خادمی افتخار آموزش در کنار این مجموعه قدرتمند را دارم</h4>
+
+  </>
+    )
+  }
+}
+export default AboutUs;
